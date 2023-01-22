4f78a06858c6374afc6b620e6fcc5e6253da4919
Jay <roebuck.jayc@gmail.com>
2023-01-22 05:41:09 -0500

    added sorting type for twitter feed

---
+++ cross-cast/config/integrations.config.ts
@@ -0,0 +1,9 @@
+type IntegrationsConfig = {
+    maxCountDefault: string
+  }
+  
+let integrationsConfig: IntegrationsConfig = {
+  maxCountDefault: "5"
+}
+  
+export default integrationsConfig;
--- cross-cast/config/twitter.config.ts
+++ cross-cast/config/twitter.config.ts
@@ -1,11 +1,9 @@
 type TwitterConfig = {
   user: string
-  maxCountDefault: string
 }
 
 let twitterConfig: TwitterConfig = {
   user: "potus",
-  maxCountDefault: "5"
 }
 
 export default twitterConfig;
